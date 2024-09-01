46f23240ddd4caf1b8bbbac54b52c9a2432fdb05
DevanDewet <161315233+DevanDewet@users.noreply.github.com>
2024-09-01 22:45:39 +0200

    updatae

--- extract_and_convert_floats.asm
+++ extract_and_convert_floats.asm
@@ -3,7 +3,26 @@
 ; Group member 02: Tafara_Hwata_22565991
 ; Group member 03: Devan_Dewet_05169098
 ; ==========================
+
+
+section .data
+    prompt db "Enter values separated by whitespace and enclosed in pipes (|):", 0
+
+section .bss
+    buffer resb 256
+    numFloats resd 1
+    floats resq 1
+
 section .text
+    extern printf, fgets, malloc, strtof
     global extractAndConvertFloats
 
 extractAndConvertFloats:
+    ; Print prompt
+    push prompt
+    call printf
+    add rsp, 8
+
+    ; Read input
+    push 256
+    push buffer
